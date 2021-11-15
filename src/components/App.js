3bee035138e97c7812c4eb902ef733bfd17721af
Aaron Marks <me@aaronmarks.dev>
2021-11-15 10:06:16 -0500

    Refactor. Use setInterval and clean up timer toggle.

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,22 @@ const App = () => {
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
-    if (isActive && seconds > 0) {
-      const timeOut = setTimeout(() => setSeconds(seconds - 1), 1000);
+    if (!seconds) {
+      setIsActive(false);
+      return;
+    }
+
+    if (isActive && seconds) {
+      const interval = setInterval(() => setSeconds(seconds - 1), 1000);
 
       return () => {
-        clearTimeout(timeOut);
+        clearInterval(interval);
       };
-    } else {
-      if (seconds === 0) {
-        setIsActive(false);
-      }
     }
   }, [seconds, isActive]);
 
-  const onStartClick = () => {
-    setIsActive(true);
-  };
-
-  const onStopClick = () => {
-    setIsActive(false);
+  const toggleTimer = () => {
+    setIsActive(!isActive);
   };
 
   return (
@@ -31,15 +28,16 @@ const App = () => {
       <h1>{ seconds }</h1>
       <div className="ui form">
         <div class="ui right labeled input">
-          <input type="text" placeholder="mysite.com" value={seconds} onChange={(e) => setSeconds(e.target.value)} />
+          <input type="number" disabled={ isActive ? 'disabled' : '' }  value={seconds} onChange={(e) => setSeconds(e.target.value)} />
           <div class="ui label">
             Seconds
           </div>
         </div>
-        <div class="ui divider"></div>
-        <div className="field">
-          <button className={`ui primary button ${ isActive ? 'disabled' : '' }`} onClick={onStartClick}>Start Timer</button>
-          <button className={`ui red button ${ !isActive ? 'disabled' : '' }`} onClick={onStopClick}>Stop Timer</button>
+        <div className="ui divider"></div>
+        <div className='field'>
+          <button className={'ui primary button'} onClick={toggleTimer}>
+            {!isActive ? 'Start' : 'Stop'} Timer
+          </button>
         </div>
       </div>
     </div>
